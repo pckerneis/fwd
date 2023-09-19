810856bfd5231b08c68da7942f004eb14e0a0d7e
pckerneis <pc.kerneis@gmail.com>
2023-09-19 21:26:22 +0200

    Remove "run" command

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ const program = new Command();
 program
   .name('conductor')
   .version('0.0.1')
-  .description('CLI to run musical programs in JS');
-
-program
-  .command('run')
-  .description('Run a program from a JS file')
+  .description('CLI to run musical programs in JS')
   .option('--file, -f <file>', 'Path to the program file to run')
   .option('--output, -o <output>', 'MIDI output to use')
   .action(async (options) => {
