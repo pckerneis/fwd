b161693b30b8a52e54e8a574788edbdff764169e
pckerneis <pc.kerneis@gmail.com>
2023-09-26 16:54:01 +0200

    generate-doc : remove top-level awat

--- src/generate-doc.js
+++ src/generate-doc.js
@@ -1,9 +1,13 @@
 import jsdoc2md from 'jsdoc-to-markdown';
 import fs from 'fs';
 
-const inputFile = 'src/modules/api.js';
-const outputFile = 'docs/api-reference.md';
-const template = fs.readFileSync('src/api-template.hbs', 'utf-8');
+async function run() {
+    const inputFile = 'src/modules/api.js';
+    const outputFile = 'docs/api-reference.md';
+    const template = fs.readFileSync('src/api-template.hbs', 'utf-8');
+    
+    const output = await jsdoc2md.render({ files: inputFile, template });
+    fs.writeFileSync(outputFile, output);
+}
 
-const output = await jsdoc2md.render({ files: inputFile, template });
-fs.writeFileSync(outputFile, output);
+run();
