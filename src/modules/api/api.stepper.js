aa24ebad806d18b39a2d0ef017b668d389d487b5
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2023-10-22 21:55:24 +0200

    Stepper single handler (#21)
    * stepper: use a single handler
    
    * stepper: parse int symbols
    
    * stepper: circumvent type error
    
    * stepper: fix tests

--- src/modules/api/api.stepper.js
+++ src/modules/api/api.stepper.js
@@ -1,8 +1,26 @@
 /**
  * @typedef {Object} Stepper
- * @property {function} at - Trigger a stepper handler function at the given step
+ * @property {function} at - Calls the handler function for the given step index
  */
 
+/**
+ * @typedef {Object} StepHandlerParameters
+ * @property {number} duration - The duration of the step
+ * @property {string|number} symbol - The symbol of the step as a number (integer) or a string (character)
+ * @property {number} line - The line of the step
+ */
+
+/**
+ * @callback StepHandler
+ * @param {StepHandlerParameters} step - The step object
+ */
+
+/**
+ * @ignore
+ * Removes whitespaces from a string
+ * @param {string} line - The string to remove whitespaces from
+ * @returns {string} the string without whitespaces
+ */
 function removeWhitespaces(line) {
   return line.replace(/\s/g, '');
 }
@@ -10,20 +28,18 @@ function removeWhitespaces(line) {
 /**
  * Creates a stepper object
  * @param {string} pattern - The pattern to play
- * @param {*} mapper - A dictionary mapping each step value to a handler function
+ * @param {StepHandler} handler - A function to be called for each step
  * @param {string} [continuation='~'] - The step continuation character
+ * @param {string} [silence='_'] - The step silence character
  * @returns {Stepper} the stepper object
  */
-export function stepper(pattern, mapper, continuation = '~') {
+export function stepper(pattern, handler, continuation = '~', silence = '_') {
   const lines = pattern.split('\n').map((line) => removeWhitespaces(line));
 
   const maxLineLength = Math.max(...lines.map((line) => line.length));
 
   const getSymbols = (index) =>
-    lines.map((line) => line[index % maxLineLength]);
-
-  const getHandlers = (index) =>
-    getSymbols(index).map((symbol) => mapper[symbol]);
+    lines.map((line) => line.charAt(index % maxLineLength));
 
   const getStepDuration = (index, lineIndex) => {
     const line = lines[lineIndex];
@@ -44,14 +60,24 @@ export function stepper(pattern, mapper, continuation = '~') {
 
   return {
     at: (index) => {
-      const handlers = getHandlers(index);
+      const symbols = getSymbols(index);
 
-      handlers.forEach((handler, handlerIndex) => {
-        if (typeof handler === 'function') {
-          const duration = getStepDuration(index, handlerIndex);
-          const symbol = getSymbols(index)[handlerIndex];
-          const line = handlerIndex;
-          handler({ duration, symbol, line });
+      symbols.forEach((symbol, line) => {
+        if (
+          symbol !== continuation &&
+          symbol !== silence &&
+          symbol != null &&
+          symbol.length > 0
+        ) {
+          const duration = getStepDuration(index, line);
+
+          handler({
+            duration,
+            symbol: '0123456789'.includes(symbol)
+              ? parseInt(symbol, 10)
+              : symbol,
+            line,
+          });
         }
       });
     },
