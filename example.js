4f9c9270e2a7047b9d263a208ef72d76e683e12d
pckerneis <pc.kerneis@gmail.com>
2023-10-22 23:30:57 +0200

    Update example file

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 // Log some text
 log('Hello, World!');
 
-// Play a MIDI note
+// Play a MIDI note with note number 36, velocity 127, and duration 1
 note(36, 127, 1);
 
 // Offset time cursor
@@ -12,7 +12,8 @@ wait(2);
 flog('Waited for 2. Now is ' + cursor());
 
 // Play another MIDI note 2 seconds after the first one
-note(40, 127, 1);
+// The fourth argument (optional) is the MIDI channel
+note(40, 127, 1, 1);
 
 // Move time cursor 1 second after current clock position
 // Try saving the program to trigger the following instructions again
@@ -25,5 +26,5 @@ next(4);
 // Repeat an action 5 times every 0.25 seconds
 repeat(0.25, (i) => log('Repeat #' + i), 5);
 
-// Sets the default MIDI channel to 9 for subsequent calls to `note`
+// Sets the default MIDI channel to 9 for subsequent calls to `note` without a channel argument
 channel(9);
